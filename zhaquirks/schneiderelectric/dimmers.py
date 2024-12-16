ff8189743ec53cf2b6511b8bfb5fb8ebe6ae4bb5
cfulton <chrisfulton1@bigpond.com>
2024-12-16 16:32:23 +1100

    Added Clipsal Dimmer and 2A Switch with attributes

--- zhaquirks/schneiderelectric/dimmers.py
+++ zhaquirks/schneiderelectric/dimmers.py
@@ -1,6 +1,9 @@
 """Schneider Electric dimmers and switches quirks."""
 
 from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.quirks.v2.homeassistant import UnitOfTime
+from zigpy.quirks.v2.homeassistant.number import NumberDeviceClass
+from zigpy.zcl import ClusterType
 
 from zhaquirks.schneiderelectric import (
     SE_MANUF_NAME,
@@ -8,26 +11,78 @@ from zhaquirks.schneiderelectric import (
     SEBasic,
     SEOnOff,
     SESpecific,
+    SEControlMode,
 )
 
 (
+    # Note: UNIDIM and DIMMER have unique setttings in Ballast Cluster.
     QuirkBuilder(SE_MANUF_NAME, "NHROTARY/DIMMER/1")
     .applies_to(SE_MANUF_NAME, "NHROTARY/UNIDIM/1")
     .applies_to(SE_MANUF_NAME, "NHPB/DIMMER/1")
     .applies_to(SE_MANUF_NAME, "NHPB/UNIDIM/1")
+    .applies_to(SE_MANUF_NAME, "CH/DIMMER/1")
     .replaces(SEBasic, endpoint_id=3)
     .replaces(SEBallast, endpoint_id=3)
     .replaces(SEOnOff, endpoint_id=3)
     .replaces(SEBasic, endpoint_id=21)
+    .replaces(SEOnOff, endpoint_id=21, cluster_type=ClusterType.Client)
     .replaces(SESpecific, endpoint_id=21)
+    .number(
+        attribute_name=SEOnOff.AttributeDefs.se_on_time_reload.name,
+        cluster_id=SEOnOff.cluster_id,
+        endpoint_id=3,
+        min_value=0x0,
+        max_value=0xFFFFFFFF,
+        step=1,
+        device_class=NumberDeviceClass.DURATION,
+        unit=UnitOfTime.SECONDS,
+        fallback_name="On Time Reload",
+        translation_key="on_time_reload")
+    .number(
+        attribute_name=SEBallast.AttributeDefs.min_level.name,
+        cluster_id=SEBallast.cluster_id,
+        endpoint_id=3,
+        min_value=1,
+        max_value=254,
+        step=1,
+        fallback_name="Min Light Level",
+        translation_key="min_level")
+    .number(
+        attribute_name=SEBallast.AttributeDefs.max_level.name,
+        cluster_id=SEBallast.cluster_id,
+        endpoint_id=3,
+        min_value=1,
+        max_value=254,
+        step=1,
+        fallback_name="Max Light Level",
+        translation_key="max_level")
+    .enum(
+        attribute_name=SEBallast.AttributeDefs.se_control_mode.name,
+        enum_class=SEControlMode,
+        cluster_id=SEBallast.cluster_id,
+        endpoint_id=3,
+        translation_key="control_mode",
+        fallback_name="Control Mode")
     .add_to_registry()
 )
 
 (
     QuirkBuilder(SE_MANUF_NAME, "NHPB/SWITCH/1")
-    .replaces(SEBasic)
-    .replaces(SEOnOff)
+    .applies_to(SE_MANUF_NAME, "CH2AX/SWITCH/1")
+    .replaces(SEBasic, endpoint_id=1)
+    .replaces(SEOnOff, endpoint_id=1)
     .replaces(SEBasic, endpoint_id=21)
     .replaces(SESpecific, endpoint_id=21)
+    .number(
+        attribute_name=SEOnOff.AttributeDefs.se_on_time_reload.name,
+        cluster_id=SEOnOff.cluster_id,
+        endpoint_id=1,
+        min_value=0x0,
+        max_value=0xFFFFFFFF,
+        step=1,
+        unit=UnitOfTime.SECONDS,
+        device_class=NumberDeviceClass.DURATION,
+        fallback_name="On Time Reload",
+        translation_key="on_time_reload")
     .add_to_registry()
 )
